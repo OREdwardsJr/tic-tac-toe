47dc5d7717b3e03e5c6e027b5032e85013fceaf7
Orlando <oredwardsjr@gmail.com>
2022-03-18 13:02:16 -0700

    Gameplay styling added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,7 @@
 const hideContent = document.querySelectorAll(".hide-content")
 const startGameButton = document.querySelector("#button-start-game")
 const launchPageContainer = document.querySelector(".launch-page-container")
+const launchGamePlay = document.querySelector("#gameplay-styling");
 
 for (let i = 0; i < hideContent.length; i++) {
     startGameButton.addEventListener("click", () => {
@@ -11,4 +12,5 @@ for (let i = 0; i < hideContent.length; i++) {
 
 startGameButton.addEventListener("click", () => {
     launchPageContainer.style = "width: 80%";
-})
+    launchGamePlay.style = "visibility: normal; position: absolute; height: inherit";
+});
