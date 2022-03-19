b6a3bf6d5b4acb204c08f1ae2d825054752a5d77
Orlando <oredwardsjr@gmail.com>
2022-03-18 19:36:16 -0700

    Basic functionality achieved. Full games can be played while score is being tracked.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,6 +3,9 @@ const hideContent = document.querySelectorAll(".hide-content")
 const startGameButton = document.querySelector("#button-start-game")
 const launchPageContainer = document.querySelector(".launch-page-container")
 const launchGamePlay = document.querySelector("#gameplay-styling");
+const newGameButton = document.querySelector(".button-new-game");
+let player1score = document.querySelector("#player1-score");
+let player2score = document.querySelector("#player2-score");
 
 for (let i = 0; i < hideContent.length; i++) {
     startGameButton.addEventListener("click", () => {
@@ -68,7 +71,7 @@ const takeTurn = (button, buttonID) => {
         button.style = "background-color: rgba(242, 0, 0, .2);"
         player = "Player 2"
     };
-    ++rounds
+
     switch (buttonID) { //This executes as expected
         case 0:
             methods[0] += parseInt(button.value);
@@ -113,23 +116,46 @@ const takeTurn = (button, buttonID) => {
             methods[6] += parseInt(button.value);
             break;
     };
+    ++rounds
 };
 
 function checkGame () {
     for (let j = 0; j < methods.length; j++) {
         console.log(methods[j])
         if (methods[j] === 3) {
-            alert("Player 1 Wins!")
+            alert("Player 1 Wins!");
+            resetGame();
+            player1score.textContent = parseInt(player1score.textContent) + 1;
         } else if (methods[j] === -3) {
-            alert("Player 2 Wins")
+            alert("Player 2 Wins");
+            resetGame();
+            player2score.textContent = parseInt(player2score.textContent) + 1;
+        } else if (rounds == 9) { 
+            alert("Tie game!");
+            resetGame();
+            break;
         };
     };
 };
 
 for (let i = 0; i < gamePieces.length; i++) {
     gamePieces[i].addEventListener("click", () => {
-        /*gamePieces[i].disabled = "true"*/
+        gamePieces[i].disabled = "true"
         takeTurn(gamePieces[i], i)
         checkGame()
     });
 };
+
+function resetGame () {
+    for (let k = 0; k < gamePieces.length; k++) {
+        gamePieces[k].value = 0;
+        gamePieces[k].textContent = "";
+        gamePieces[k].style = "background-color: var(--launch-secondary-color";
+        gamePieces[k].disabled = false;
+    }
+    for (let l = 0; l < methods.length; l++) {
+        methods[l] = 0;
+    }
+}
+
+newGameButton.addEventListener("click", () => resetGame());
