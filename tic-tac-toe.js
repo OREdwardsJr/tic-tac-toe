2ea2263d595959daa5f45cc2bcb2524e46600ea5
Orlando <oredwardsjr@gmail.com>
2022-03-18 19:06:59 -0700

    Functionality update. Score is now accurately tracked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -18,30 +18,118 @@ startGameButton.addEventListener("click", () => {
 
 // Game Controls
 // Note to self - add underline to indicate turns
+let rounds = 0
+const gamePieces = document.querySelectorAll(".game-pieces");
+
+//Winning methods:
+/* To the right of methods is a coordinate. If you consider the board to be a plot then
+you can find the piece using the coordinates. The first row indicates x = 0. The first
+column equals to y = 0. EG: (0,0) is the first square in the  first column on the first row. */
+
+/* Legend
+    Coordinate | Button ID{i} | Methods
+    (0,0) | 0 | 00, 03, 06
+    (0,1) | 1 | 00, 04
+    (0,2) | 2 | 00, 03, 05
+    (1,0) | 3 | 01, 03
+    (1,1) | 4 | 01, 04, 06, 07
+    (1,2) | 5 | 01, 05
+    (2,0) | 6 | 02, 03, 07
+    (2,1) | 7 | 02, 04
+    (2,2) | 8 | 02, 05, 06
+
+
+methods[0] = 0, // (0,0), (0,1), (0,2)
+methods[1] = 0, // (1,0), (1, 1), (1,2)
+methods[2] = 0, // (2,0), (2,1), (2,2)
+methods[3] = 0, // (0,0), (1,0), (2,0)
+methods[4] = 0, // (0,1), (1,1), (2,1)
+methods[5] = 0, // (0,2), (1,2), (2,2)
+methods[6] = 0, // (0,0), (1,1), (2,2)
+methods[7] = 0; // (2,0), (1,1), (0,2)  
+*/
+
+
+let methods = new Array(0, 0, 0, 0, 0, 0, 0, 0);
+
 
 // Factory function
-let rounds = 0
-const takeTurn = (button) => {
-
+const takeTurn = (button, buttonID) => {
+    let player;
+    button.disabled = "true";
     if (rounds % 2 === 0) {
         button.textContent = "X"
         button.value = 1
         button.style = "background-color: rgba(51, 170, 51, .2);"
+        player = "Player 1"
     } else {
         button.textContent = "O"
         button.value = -1
         button.style = "background-color: rgba(242, 0, 0, .2);"
+        player = "Player 2"
     };
-    rounds = ++rounds
-    console.log(button.textContent, button.value)
-    return {/* x or o, trigger for game over, trigger for winner */}
+    ++rounds
+    switch (buttonID) { //This executes as expected
+        case 0:
+            methods[0] += parseInt(button.value);
+            methods[3] += parseInt(button.value);
+            methods[6] += parseInt(button.value);
+            break;
+        case 1:
+            methods[0] += parseInt(button.value);
+            methods[4] += parseInt(button.value);
+            break;
+        case 2:
+            methods[0] += parseInt(button.value);
+            methods[3] += parseInt(button.value);
+            methods[5] += parseInt(button.value);
+            break;
+        case 3:
+            methods[1] += parseInt(button.value);
+            methods[3] += parseInt(button.value);
+            break;
+        case 4:
+            methods[1] += parseInt(button.value);
+            methods[4] += parseInt(button.value);
+            methods[6] += parseInt(button.value);
+            methods[7] += parseInt(button.value);
+            break;
+        case 5:
+            methods[1] += parseInt(button.value);
+            methods[5] += parseInt(button.value);
+            break;
+        case 6:
+            methods[2] += parseInt(button.value);
+            methods[3] += parseInt(button.value);
+            methods[7] += parseInt(button.value);
+            break;
+        case 7:
+            methods[2] += parseInt(button.value);
+            methods[4] += parseInt(button.value);
+            break;
+        case 8:
+            methods[2] += parseInt(button.value);
+            methods[5] += parseInt(button.value);
+            methods[6] += parseInt(button.value);
+            break;
+    };
 };
 
-const gamePieces = document.querySelectorAll(".game-pieces");
+function checkGame () {
+    for (let j = 0; j < methods.length; j++) {
+        console.log(methods[j])
+        if (methods[j] === 3) {
+            alert("Player 1 Wins!")
+        } else if (methods[j] === -3) {
+            alert("Player 2 Wins")
+        };
+    };
+};
 
 for (let i = 0; i < gamePieces.length; i++) {
     gamePieces[i].addEventListener("click", () => {
-        gamePieces[i].disabled = "true"
-        takeTurn(gamePieces[i])
+        /*gamePieces[i].disabled = "true"*/
+        takeTurn(gamePieces[i], i)
+        checkGame()
     });
 };
